c7c6d980208111a60e42492a70fcbb4cb5970fb2
Thomas van Dam <thomasvandam@gmail.com>
2022-09-16 09:30:20 -0400

    Add lesson 3: Menu extension

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,11 +1,12 @@
 import shutil
 import sys
-
-from typing import List
 from pathlib import Path
+from typing import List
 
 from docutils.core import publish_parts
 from markdown import markdown
+
+from ssg import hooks
 from ssg.content import Content
 
 
@@ -45,10 +46,9 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(path, dest, html)
-        sys.stdout.write(
-            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
-        )
+        filtered = hooks.filter("generate_menu", html, self.base_ext)
+        self.write(path, dest, filtered)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
 
 
 class ReStructuredTextParser(Parser):
@@ -57,7 +57,6 @@ class ReStructuredTextParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = publish_parts(content.body, writer_name="html5")
-        self.write(path, dest, html["html_body"])
-        sys.stdout.write(
-            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
-        )
+        filtered = hooks.filter("generate_menu", html["html_body"], self.base_ext)
+        self.write(path, dest, filtered)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
